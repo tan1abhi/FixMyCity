e13434c41a4f811893d2793834352d08a65ad224
Sriya Singh <133565083+sriya-singh@users.noreply.github.com>
2025-03-31 15:20:12 +0530

    Update app.py

--- sentiment_analysis/app.py
+++ sentiment_analysis/app.py
@@ -9,7 +9,20 @@ from firebase_admin import credentials, firestore, initialize_app
 load_dotenv()
 
 # Get the Firebase credentials path from the environment
-firebase_credentials_path = os.environ.get("FIREBASE_CREDENTIALS_PATH")
+firebase_credentials = {
+    "type": os.getenv("FIREBASE_TYPE"),
+    "project_id": os.getenv("FIREBASE_PROJECT_ID"),
+    "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
+    "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace('\\n', '\n'),
+    "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
+    "client_id": os.getenv("FIREBASE_CLIENT_ID"),
+    "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
+    "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
+    "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_X509_CERT_URL"),
+    "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_X509_CERT_URL"),
+    "universe_domain": os.getenv("FIREBASE_UNIVERSE_DOMAIN"),
+}
+
 
 app = Flask(__name__)
 
@@ -17,7 +30,7 @@ app = Flask(__name__)
 CORS(app)
 
 # Init Firebase Admin SDK
-cred = credentials.Certificate(firebase_credentials_path)  # Add your Firestore service account key
+cred = credentials.Certificate(firebase_credentials)  # Add your Firestore service account key
 initialize_app(cred)
 db = firestore.client()
 
