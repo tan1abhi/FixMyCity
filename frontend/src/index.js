a57bf655efd2025613d95917cb8e1b7903550cbe
Sriya Singh <133565083+sriya-singh@users.noreply.github.com>
2025-03-31 22:51:41 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -14,7 +14,7 @@ root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <BrowserRouter>
+      <BrowserRouter basename="/">
         {/* ✅ Show "Loading..." while App is loading */}
         <Suspense fallback={<div>Loading...</div>}>
           <App />
