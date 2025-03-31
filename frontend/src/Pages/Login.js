b7fab166ba4beea0fd1109f799eb360231b9b285
Sriya Singh <133565083+sriya-singh@users.noreply.github.com>
2025-03-31 23:00:01 +0530

    Update Login.js

--- frontend/src/Pages/Login.js
+++ frontend/src/Pages/Login.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { useNavigate, Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
 import { handleError, handleSuccess } from './utils'
 import { createTheme, ThemeProvider } from '@mui/material/styles';
